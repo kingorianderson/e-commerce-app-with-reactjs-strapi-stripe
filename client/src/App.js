4626eda41fda0bec15737c7ca31196e381bf837f
kingorianderson <kingorianderson@gmail.com>
2022-11-27 14:37:20 +0300

    client

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>,
+  },
+  {
+    path: "/products/:id",
+    element: <span>Category</span>,
+  },
+  {
+    path: "/product/:id",
+    element: <span>Product</span>,
+  },
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
